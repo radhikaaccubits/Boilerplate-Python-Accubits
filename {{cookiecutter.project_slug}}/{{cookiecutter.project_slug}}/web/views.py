21b8eb1b064b6203cf2304badf469f220f5d23bf
ganeshkg <ganesh.kg@accbits.com>
2022-08-10 14:46:37 +0530

    formatted code in models.py, forms and views

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web/views.py
@@ -23,14 +23,14 @@ class IndexView(generic.TemplateView):
 
 class CreateUser(View):
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         profileform = CreateProfileForm()
         userform = CreateUserForm()
         return render(request, 'users/create.html', {
             'profileform': profileform,
             'userform': userform, })
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         profileform = CreateProfileForm(request.POST)
         userform = CreateUserForm(request.POST)
         if profileform.is_valid() and userform.is_valid():
@@ -38,18 +38,16 @@ class CreateUser(View):
             user = userform.save()
             profile.user = user
             profile.save()
-
         else:
-
             return render(request, 'users/create.html', {'profileform': profileform, 'userform': userform})
         messages.success(request, 'The record was saved successfully')
         return redirect('/')
 
 
 class UpdateView(UpdateView):
+
     def get(self, request, *args, **kwargs):
         post = UserProfile.objects.get(id=kwargs.get('post_id'))
-
         userform = CreateUserForm(instance=post.user)
         profileform = CreateProfileForm(instance=post)
         context = {
@@ -58,8 +56,8 @@ class UpdateView(UpdateView):
             'post_id': post.id, }
         return render(request, 'users/update.html', context)
 
-    def post(self, request, post_id):
-        req = request.POST
+    def post(self, request, *args, **kwargs):
+        post_id = kwargs.get('post_id')
         post = models.UserProfile.objects.get(id=post_id)
         profile_form = CreateProfileForm(request.POST, instance=post)
         user_form = CreateUserForm(request.POST, instance=post.user)
@@ -67,17 +65,19 @@ class UpdateView(UpdateView):
             profile_form.save()
             user_form.save()
         else:
-
             return render(request, 'users/update.html',
-                          {'profileform': profile_form, 'userform': user_form, 'post_id': post.id, })
+                          {'profileform': profile_form,
+                           'userform': user_form,
+                           'post_id': post.id, })
         messages.success(request, 'The record was saved successfully')
         return redirect('/')
 
 
 class DeleteView(View):
+
     def post(self, request, *args, **kwargs):
-        deleteid = request.POST.get('del')
-        post = UserProfile.objects.get(id=deleteid)
+        delete_id = request.POST.get('del')
+        post = UserProfile.objects.get(id=delete_id)
         post.user.is_active = False
         post.user.save()
         post.save()
